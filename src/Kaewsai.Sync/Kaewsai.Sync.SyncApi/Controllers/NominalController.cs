e6ff8bac2914b019a1e90ab81c4b72b260cc7446
Mattanapol Konguthaikul <mattanapol.k@hotmail.com>
2021-11-27 19:01:43 +0700

    Remove unused imports

--- src/Kaewsai.Sync/Kaewsai.Sync.SyncApi/Controllers/NominalController.cs
+++ src/Kaewsai.Sync/Kaewsai.Sync.SyncApi/Controllers/NominalController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Kaewsai.Sync.SyncApi.Domain.Model.Nominal;
 using Kaewsai.Sync.SyncApi.Domain.Repository;
